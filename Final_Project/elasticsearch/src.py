e2e05ec8633e42766e724e35fc7e63870ac6ddd9
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-05-28 21:54:57 +0900

    Update src.py

--- Final_Project/elasticsearch/src.py
+++ Final_Project/elasticsearch/src.py
@@ -1,8 +1,17 @@
-#!/bin/usr/python
-from elasticsearch import Elasticsearch as es
+#!/usr/bin/python
+import get_data
 
 """
-0 : 예외, 1 : 반팔 티셔츠, 2 : 긴팔 티셔츠, 3 : 맨투맨, 4 : 셔츠, 5 : 블라우스, 6 : 니트, 7 : 민소매 티셔츠, 8 : 청바지, 9 : 슬랙스, 10 : 면바지
+0 : 예외
+
+남자
+1 : 반팔 티셔츠, 2 : 긴팔 티셔츠, 3 : 코트, 4 : 셔츠, 5 : 블라우스(자켓), 6 : 니트(스웨터), 7 : 민소매 티셔츠(탱크톱), 8 : 패딩
+1 : 청바지, 2 : 트레이닝 바지, 3 : 슬랙스, 4 : 면바지, 5 : 반바지
+
+여자
+1 : 반팔 티셔츠, 2 : 긴팔 티셔츠, 3 : 코트, 4 : 셔츠, 5 : 블라우스(자켓), 6 : 니트(스웨터), 7 : 민소매 티셔츠(탱크톱), 8 : 패딩
+1 : 청바지, 2 : 트레이닝 바지, 3 : 슬랙스, 4 : 면바지, 5 : 반바지, 6 : 롱스커트, 7 : 레깅스, 8 : 미니스커트
+
 맑음(맑), 구름(구), 흐림(흐), 비(비), 소나기(소), 눈(눈)
 """
 
@@ -48,6 +57,8 @@ def set_table() -> dict:
     return d
 
 
+gender = input()
+
 temperature = int(input())      # 기온을 입력 받음, 수정
 temperature_index = temperature_index_creator(temperature)     # 기온을 바탕으로 기온의 인덱스를 받아옴
 
@@ -67,4 +78,6 @@ table = set_table()
 
 key = table[(weather_index, temperature_index)]
 
-print(key)
+data = get_data.get_data(gender, (1, 8))        # 데이터를 받아옴
+
+print(data)
