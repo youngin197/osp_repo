e67332ceb51c496047acef07d76bbde97e73b62e
osmin625 <tmdals179@gmail.com>
2022-06-08 02:24:12 +0900

    from scratch

--- Final_Project/run.sh
+++ Final_Project/run.sh
@@ -3,7 +3,9 @@
 ##### 8조 기말 프로젝트 실행 쉘 파일 #####
 
 # 1)라이브러리 설치
-플라스크, 엘라스틱서치, 뷰티풀수프, 리퀘스츠
+=======
+# 플라스크, 엘라스틱서치, 뷰티풀수프, 리퀘스츠
+>>>>>>> from scratch
 #sudo apt-get install python-pip
 sudo apt update
 sudo apt install elasticsearch
@@ -11,11 +13,13 @@ pip install flask
 pip install beautifulsoup4
 pip install requests
 
-# 2)플라스크 실행
-#flask run
+# 1)엘라스틱 서치 실행
+./elasticsearch-8.2.2/bin/elasticsearch -d
 
-# 3)엘라스틱 서치 실행
-./bin/elasticsearch -d
+# 2) 데이터 생성
+./insert_data.py
 
-# 4)app.py 실행
-python3 app.py
+# 3)플라스크 실행
+flask run
+
+>>>>>>> from scratch
