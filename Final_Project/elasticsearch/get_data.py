35b2e499e996e541b9e831525f1ce7848b61a75b
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-06-07 15:58:46 +0900

    Update get_data.py

--- Final_Project/elasticsearch/get_data.py
+++ Final_Project/elasticsearch/get_data.py
@@ -17,35 +17,55 @@
 import sys, json
 from elasticsearch import Elasticsearch
 
-def get_data(gender: str, fashion: tuple) -> dict:
+def get_data(gender: int, fashion: list) -> dict:
     # 성별, 아이디, 상하의 정보를 받아서 url을 반환하는 함수
     
     es_host = "http://localhost:9200"
     es = Elasticsearch(es_host)
 
+    if type(gender) == int:
+        if gender == 0:
+            gender = 'man'
+        elif gender == 1:
+            gender = 'woman'
+
     index_name_top = gender + '_' + 'top'       # 성별_상의
     index_name_bottom = gender + '_' + 'bottom'     # 성별_하의
 
-    query_top = {"query":{"match":{"id":f"{fashion[0]}"}}}      # 상의 쿼리문
-    query_bottom = {"query":{"match":{"id":f"{fashion[1]}"}}}       # 하의 쿼리문
+    fashion_top = fashion[0]        # 하의 정보
+    fashion_bottom = fashion[1]     # 상의 정보
 
-    top_data = es.search(index=index_name_top, body=query_top, pretty=True )     # 데이터 가져옴
-    bottom_data = es.search(index=index_name_bottom, body=query_bottom, pretty=True)
+    top_data = []
+    bottom_data = []
+
+    for f in fashion_top:
+        query_top = {"query":{"match":{"id":f"{f}"}}}      # 상의 쿼리문
+        top_data.append(es.search(index=index_name_top, body=query_top, pretty=True ))     # 데이터 가져옴
+
+    for f in fashion_bottom:
+        query_bottom = {"query":{"match":{"id":f"{f}"}}}       # 하의 쿼리문
+        bottom_data.append(es.search(index=index_name_bottom, body=query_bottom, pretty=True))        # 데이터 가져옴
+
+    # print(top_data)
     
-    top_data = top_data['hits']['hits']     # 데이터 변환
-    bottom_data = bottom_data['hits']['hits']
+    top_data = list(map(lambda x : x['hits']['hits'], top_data))     # 데이터 변환
+    bottom_data = list(map(lambda x : x['hits']['hits'], bottom_data))
 
-    data = {}
+    # print(top_data[1])
 
-    for source in top_data:     # id, 이름, 주소를 data 딕셔너리에 저장함
-        data['top'] = source['_source']
-    for source in bottom_data:
-        data['bottom'] = source['_source']
+    data = {'top' : [], 'bottom' : []}
+
+    for a in top_data:     # id, 이름, 주소를 data 딕셔너리에 저장함
+        for source in a:
+            data['top'].append(source['_source'])
+    for a in bottom_data:
+        for source in a:
+            data['bottom'].append(source['_source'])
 
     
     return data
   
 
-
 if __name__ == "__main__":
-    get_data('woman', (1, 8))
+    data = get_data('woman', [[1, 2], [8]])
+    print(data['top'])
