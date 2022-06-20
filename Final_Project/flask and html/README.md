0c87e4d66d753c84c1d7d163e6b8f82b74f91c4c
osmin625 <tmdals179@gmail.com>
2022-06-20 14:49:30 +0900

    add css

--- Final_Project/flask and html/README.md
+++
@@ -1,3 +0,0 @@
-<!-- sh 파일 작업을 위한 명령어 모아놓기 -->
-<!-- sudo apt install python3-pip -->
-<!-- /bin/python3 -m pip install -U autopep8 -->
--- Final_Project/run.sh
+++ Final_Project/run.sh
@@ -3,23 +3,18 @@
 ##### 8조 기말 프로젝트 실행 쉘 파일 #####
 
 # 1)라이브러리 설치
-=======
 # 플라스크, 엘라스틱서치, 뷰티풀수프, 리퀘스츠
->>>>>>> from scratch
-#sudo apt-get install python-pip
+pip --version
+sudo apt-get install python3-pip
 sudo apt update
-sudo apt install elasticsearch
+pip install elasticsearch
 pip install flask
 pip install beautifulsoup4
 pip install requests
 
-# 1)엘라스틱 서치 실행
-./elasticsearch-8.2.2/bin/elasticsearch -d
-
-# 2) 데이터 생성
+# 2) 의상 데이터 생성
 ./insert_data.py
+#./app.py
 
 # 3)플라스크 실행
 flask run
-
->>>>>>> from scratch
