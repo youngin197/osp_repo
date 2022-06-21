228b04527a409e8c2396d06ac2e697ff5e5baabc
Woo <dntmdxor99@naver.com>
2022-06-22 02:00:44 +0900

    깃허브 정리

--- hw06/bin2hex.py
+++
@@ -1,6 +0,0 @@
-def bi2hex(x): 
-    x_bi = bin(x)
-    x_ten = int('0b'+ str(x), 2)
-    x_h = hex(x_ten)
-    print('hexa number:', end=' ')
-    print(x_h)
--- hw06/setop.py
+++
@@ -1,7 +0,0 @@
-def set_op(list1, list2):
-    list1 = set(list1)
-    list2 = set(list2)
-    print('union:', end=' ')
-    print(list(list1 | list2))
-    print('intersection:', end=' ')
-    print(list(list1 & list2))
